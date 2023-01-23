c0419fc6d9b22205b5b56ada934efd0ed13219bb
Jonathan Manes <manesjona@gmail.com>
2023-01-23 10:34:35 +0100

    creation Database class, setup and config file

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Database connection settings
+define('DB_HOST', 'db-todolist.cld0mu6jdw4b.eu-west-3.rds.amazonaws.com');
+define('DB_NAME', 'db-todolist');
+define('DB_USER', 'admin');
+define('DB_PASS', 'K35zNf7ACwwDSUcvh6F5');
+define('DB_PORT', '3306');
+
+// Other config options
+define('DEBUG_MODE', true);
