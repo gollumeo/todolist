64b6e36de2d83f76f732ae10035c91249e921359
Jonathan Manes <manesjona@gmail.com>
2023-01-24 16:03:11 +0100

    AVANCEMENT

--- Resources/views/shared/footer.php
+++ Resources/views/shared/footer.php
@@ -1,5 +1,5 @@
 <footer class="flex flex-row w-[100%] bg-slate-800 p-3 shadow-2xl shadow-slate-800/60 justify-center items-center">
-        <nav class="w-auto flex gap-6 text-lg items-center justify-center flex flex-col">
+        <nav class="w-auto gap-6 text-lg items-center justify-center flex flex-col">
             <div class="items gap-6 items text-sm text-center">© Jonathan Manes & Pierre Mauriello | BeCode - Keller 5 - 2023<br>All Rights Reserved<br>Base project provided by <a class="inline"href="https://github.com/pierrenoel"><strong>Pierre N.</strong></a></div>
         </nav>
     </footer>
--- Resources/views/task/show.php
+++ Resources/views/task/show.php
@@ -6,14 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/output.css">
     <script src="https://kit.fontawesome.com/26838d0dd7.js" crossorigin="anonymous"></script>
 
     <title>List of tasks</title>
 </head>
 
-<body>
+<body class="flex flex-col h-screen">
+    <?php include '../Resources/views/shared/header.php'?>
+    <main class="mb-auto mt-20">
     <h2 class="text-2xl text-center">List of tasks</h2>
     <?php foreach ($task_list as $task) {
 
@@ -27,6 +28,9 @@
         echo '</form>';
         echo '</section>';
     }; ?>
+    </main>
+    <?php include '../Resources/views/shared/footer.php'?>
+
 </body>
 
 </html>
